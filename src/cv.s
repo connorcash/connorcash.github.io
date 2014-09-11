e67d15ec437c7b7333e7d1adc20ec1e5283275ff
bsmt <bsmt@krax.in>
2014-09-11 17:24:52 -0600

    Fix cv.s

--- src/cv.s
+++ src/cv.s
@@ -1,17 +1,15 @@
-# as -arch x86_64 -o cv.o
-# ld -e _main -arch x86_64 -o cv cv.o
-.data
+# as -arch x86_64 -o cv.o cv.s
+# ld -arch x86_64 -lSystem -o cv cv.o
+
+.cstring
 greet:
-    .ascii "Hello, my name is Connor. If I do anything notable, it'll be listed below.\10\0"
-greet_len:
-    .long . - greet
+    .ascii "Hello, my name is Connor. If I do anything notable, it'll be listed below."
 
 .text
 .globl _main
 _main:
-    movq $4, %rax  # SYS_write - OS X
-    movq $1, %rdi  # stdout
-    movq greet@GOTPCREL(%rip), %rsi
-    syscall
-    xorq %rax, %rax
+    subq $8, %rsp
+    leaq greet(%rip), %rdi
+    call _printf
+    addq $8, %rsp
     ret
