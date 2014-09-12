c70e8c360888a3e2ea9b755200b1b4f6641cf5e9
bsmt <bsmt@krax.in>
2014-09-11 19:15:22 -0600

    try to make things cleaner

--- src/cv.s
+++ src/cv.s
@@ -1,9 +1,10 @@
+# to build and link on OS X:
 # as -arch x86_64 -o cv.o cv.s
 # ld -arch x86_64 -lSystem -o cv cv.o
 
 .cstring
 greet:
-    .ascii "Hello, my name is Connor. If I do anything notable, it'll be listed below."
+    .ascii "Hello, my name is Connor. If I do anything notable, it'll be listed below.\n\n"
 
 .text
 .globl _main
